589cf4f306545d64ead670a713dc8cc8f04bdc5b
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2023-05-18 21:09:32 +0300

    add MainMenu function component

--- src/App.tsx
+++ src/App.tsx
@@ -31,35 +31,40 @@ function AppHeader() {
           <span className="slider"></span>
         </label>
       </div>
-
-      <ul id="top-menu-bar">
-        <li>
-          <a href="#" data-page="home">
-            HOME
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="skills">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="languages">
-            Known Languages
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="photography">
-            Photography
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="rubik">
-            Rubik
-          </a>
-        </li>
-      </ul>
+      <MainMenu />
     </header>
+  );
+}
+
+function MainMenu() {
+  return (
+    <ul id="top-menu-bar">
+      <li>
+        <a href="#" data-page="home">
+          HOME
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="skills">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="languages">
+          Known Languages
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="photography">
+          Photography
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="rubik">
+          Rubik
+        </a>
+      </li>
+    </ul>
   );
 }
 
