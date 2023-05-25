2303488f74672f368178b8dea6ef3be6f389270f
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2023-05-25 20:39:06 +0300

    inline teams inside teams table

--- src/teams/Teams.tsx
+++ src/teams/Teams.tsx
@@ -1,6 +1,79 @@
 import "./style.css";
 
 export function Teams() {
+  const teams = [
+    {
+      id: "toze8j1610313009673",
+      promotion: "html",
+      members: "Nicolae Matei, HTML",
+      name: "Web Presentation",
+      url: "https://github.com/nmatei/web-intro-presentation"
+    },
+    {
+      id: "ezabnf1630345987541",
+      promotion: "css",
+      members: "Nicolae",
+      name: "Names",
+      url: "https://github.com/nmatei/nmatei.github.io"
+    },
+    {
+      id: "86mq81630347385708",
+      promotion: "js",
+      members: "Matei, Andrei",
+      name: "JS/HTML/CSS Quiz",
+      url: "https://github.com/nmatei/simple-quiz-app"
+    },
+    {
+      id: "lgc7c1679590352971",
+      promotion: "da",
+      members: "da",
+      name: "da",
+      url: "da"
+    },
+    {
+      id: "gqk81679591993711",
+      promotion: "ada",
+      members: "ada",
+      name: "ada",
+      url: "ada"
+    },
+    {
+      id: "13atzy1679592546309",
+      promotion: "Fio",
+      members: "Fio",
+      name: "Fio",
+      url: "Fio"
+    },
+    {
+      id: "6o490v1679592803803",
+      promotion: "Rox",
+      members: "Rox",
+      name: "Rox",
+      url: "Rox"
+    },
+    {
+      id: "bh3b61680798473339",
+      promotion: "Ana",
+      members: "are",
+      name: "mere",
+      url: "anaaremere.io"
+    },
+    {
+      id: "5w9ps1680800870404",
+      promotion: "js",
+      members: "js",
+      name: "js",
+      url: "js"
+    },
+    {
+      id: "qwhrl41680801144323",
+      promotion: "Adriana",
+      members: "are",
+      name: "mere",
+      url: "Pere"
+    }
+  ];
+
   return (
     <form id="editForm" action="" method="POST">
       <table id="teams">
@@ -13,7 +86,30 @@ export function Teams() {
             <th></th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {teams.map(({ promotion, members, name, url, id }) => (
+            <tr key={id}>
+              <td>{promotion}</td>
+              <td>{members}</td>
+              <td>{name}</td>
+              <td>
+                <a href="${url}" target="_blank">
+                  {url.replace("https://github.com/", "")}
+                </a>
+              </td>
+              <td>
+                <a data-id="${id}" className="remove-btn">
+                  {" "}
+                  ✖{" "}
+                </a>
+                <a data-id="${id}" className="edit-btn">
+                  {" "}
+                  &#9998;{" "}
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
         <tfoot>
           <tr>
             <td>
